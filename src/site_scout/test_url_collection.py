1e41578ffe0a7191ac6f698798721c3df3ba441b
Tyson Smith <tyson.w.smith@gmail.com>
2024-06-03 13:56:25 -0700

    Add url_collection.py
    UrlCollection can be used to manage YML files.

---
+++ src/site_scout/test_url_collection.py
@@ -0,0 +1,138 @@
+# type: ignore
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+# pylint: disable=missing-docstring,protected-access
+from pytest import mark, raises
+
+from .url_collection import UrlCollection, main, parse_args
+
+BASIC_URL_DB = {
+    "d1": {"*": ["/", "/d1-path1"]},
+    "d2": {
+        "sd2-1": ["/sd2-path1"],
+        "sd2-2": ["/"],
+    },
+}
+
+
+@mark.parametrize(
+    "url_str, expected",
+    [
+        ("http://domain.com/", "http://domain.com/"),
+        ("http://sub.domain.com/", "http://sub.domain.com/"),
+        ("https://sub.domain.com/", "http://sub.domain.com/"),
+        ("sub.domain.com", "http://sub.domain.com/"),
+        ("domain.com", "http://domain.com/"),
+        ("domain.co.uk", "http://domain.co.uk/"),
+        ("domain.com/test", "http://domain.com/test"),
+        ("domain.com/test?123", "http://domain.com/test?123"),
+        ("domain.com/test#123", "http://domain.com/test#123"),
+        ("HTTP://SUB.DOMAIN.COM/TEST", "http://sub.domain.com/TEST"),
+        ("failed-parse", None),
+    ],
+)
+def test_url_collection_parse_url(url_str, expected):
+    """test UrlCollection._parse_url()"""
+    parsed = UrlCollection._parse_url(url_str, default_subdomain=None)
+    if expected is None:
+        assert parsed is None
+    else:
+        assert parsed
+        assert str(parsed) == expected
+
+
+def test_url_collection_01():
+    """test empty UrlCollection()"""
+    urls = UrlCollection()
+    assert urls is not None
+    assert not urls
+    assert len(urls) == 0
+    assert not tuple(urls.domains)
+
+
+def test_url_collection_02(tmp_path):
+    """test simple UrlCollection()"""
+    urls = UrlCollection(BASIC_URL_DB)
+    assert urls
+    assert len(urls) == 4
+    assert len(tuple(urls.domains)) == 2
+    # save and load
+    yml = tmp_path / "urls.yml"
+    urls.save_yml(yml)
+    urls = UrlCollection.load_yml(yml)
+    assert urls
+    # add/remove urls
+    assert len(urls) == 4
+    assert urls.add_url("test.com")
+    assert len(urls) == 5
+    assert urls.remove_url("test.com")
+    assert not urls.remove_url("test.com")
+    # try adding a urls that can't be parsed
+    assert not urls.unparsable
+    assert not urls.add_url("invalid")
+    assert "invalid" in urls.unparsable
+    # add a list of urls from a file
+    txt = tmp_path / "urls_list.txt"
+    txt.write_text("1.test.com\n\n2.test.com\n#ignored.com\nother.com")
+    assert urls.add_list(txt)
+    assert not urls.add_list(txt)
+    assert len(urls) == 7
+
+
+def test_parse_args(capsys, tmp_path):
+    """test parse_args()"""
+    #
+    data_file = tmp_path / "test.yml"
+    args = parse_args([str(data_file)])
+    assert args.url_db == data_file
+
+    #
+    with raises(SystemExit):
+        parse_args([str(data_file), "-l", "missing_list.txt"])
+    assert "error: missing_list.txt is not a file." in capsys.readouterr().err
+
+
+def test_main(tmp_path):
+    """test main()"""
+
+    # no data file
+    data_file = tmp_path / "test.yml"
+    assert main([str(data_file)]) == 0
+    assert not data_file.exists()
+
+    # invalid data file
+    data_file.write_bytes(b"\0")
+    assert main([str(data_file)]) == 1
+    data_file.unlink()
+
+    # remove url (data file does not exists)
+    assert main([str(data_file), "-r", "test.com"]) == 0
+    assert not data_file.exists()
+
+    # add single url
+    assert main([str(data_file), "-u", "test.com", "-d"]) == 0
+    assert data_file.exists()
+
+    # remove url (data file now exists)
+    file_size = data_file.stat().st_size
+    assert main([str(data_file), "-r", "test.com"]) == 0
+    assert data_file.stat().st_size < file_size
+    assert data_file.exists()
+
+    # add unparsable
+    file_size = data_file.stat().st_size
+    assert main([str(data_file), "-u", "blah"]) == 0
+    assert data_file.stat().st_size == file_size
+
+    # remove unparsable
+    file_size = data_file.stat().st_size
+    assert main([str(data_file), "-r", "blah"]) == 0
+    assert data_file.stat().st_size == file_size
+
+    # add urls from list file
+    txt = tmp_path / "urls_list.txt"
+    txt.write_text("1.test.com\n\n2.test.com\n#ignored.com\nother.com")
+    file_size = data_file.stat().st_size
+    assert main([str(data_file), "-l", str(txt)]) == 0
+    assert data_file.stat().st_size > file_size
---
+++ src/site_scout/url_collection.py
@@ -0,0 +1,296 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+from argparse import ArgumentParser, Namespace
+from logging import DEBUG, INFO, getLogger
+from pathlib import Path
+from typing import Any, Dict, Generator, List, Optional, Set
+from urllib.parse import urlsplit
+
+from tldextract import extract
+from yaml import dump
+
+try:
+    from yaml import CSafeDumper as SafeDumper
+except ImportError:
+    from yaml import SafeDumper  # type: ignore
+
+from .main import init_logging, load_input
+from .site_scout import NO_SUBDOMAIN, URL
+
+LOG = getLogger(__name__)
+
+
+class UrlCollection:
+    """Manage collections of URLs."""
+
+    __slots__ = ("_db", "unparsable")
+
+    def __init__(self, url_db: Optional[Dict[Any, Any]] = None) -> None:
+        self._db = url_db or {}
+        self.unparsable: Set[str] = set()
+
+    def __len__(self) -> int:
+        count = 0
+        for domain in self._db:
+            for sub in self._db[domain]:
+                count += len(self._db[domain][sub])
+        return count
+
+    def __iter__(self) -> Generator[URL, None, None]:
+        for domain in self._db:
+            for sub in self._db[domain]:
+                for path in self._db[domain][sub]:
+                    yield URL(domain, subdomain=sub, path=path)
+
+    @staticmethod
+    def _parse_url(
+        url: str, default_subdomain: Optional[str] = NO_SUBDOMAIN
+    ) -> Optional[URL]:
+        """Parse URL from given string.
+
+        Args:
+            url: URL to parse.
+            default_subdomain: Placeholder for empty subdomain.
+
+        Returns:
+            URL object if successfully parsed otherwise None.
+        """
+        if "://" in url:
+            url = url.split("://", maxsplit=1)[-1]
+        parsed_url = urlsplit(f"http://{url}", allow_fragments=False)
+        # TODO: handle params? check for spaces and unsupported chars?
+        assert parsed_url.netloc
+        # URL domain info
+        udi = extract(parsed_url.netloc)
+        if not udi.domain or not udi.suffix:
+            return None
+        assert udi.domain, f"{parsed_url.netloc} -> {udi}"
+        assert udi.suffix, f"{parsed_url.netloc} -> {udi}"
+        domain = f"{udi.domain.lower()}.{udi.suffix.lower()}"
+        path = parsed_url.path.strip() or "/"
+        if parsed_url.query:
+            path = f"{path}?{parsed_url.query}"
+        return URL(
+            domain,
+            # replace empty/missing subdomain with placeholder
+            subdomain=udi.subdomain.lower() if udi.subdomain else default_subdomain,
+            path=path,
+        )
+
+    def add_list(self, urls_file: Path) -> int:
+        """Load a text file containing a line separated list of URLs and add them
+        to the collection of known URLs.
+
+        Args:
+            urls_file: File to load.
+
+        Returns:
+            Number of previously unknown URLs that have been added.
+        """
+        added = 0
+        with urls_file.open() as in_fp:
+            for line in in_fp:
+                entry = line.strip()
+                if not entry or entry[0] == "#":
+                    # skip comments and empty lines
+                    continue
+                if self.add_url(entry) is not None:
+                    added += 1
+        return added
+
+    def add_url(self, url: str) -> Optional[URL]:
+        """Load a single URL and add it to the list of known URLs.
+
+        Args:
+            url: URL to add.
+
+        Returns:
+            URL that was added.
+        """
+        parsed = self._parse_url(url)
+        if parsed is None:
+            LOG.debug("failed to parse and add: %r", url)
+            self.unparsable.add(url)
+        else:
+            if parsed.domain not in self._db:
+                self._db[parsed.domain] = {}
+            if parsed.subdomain not in self._db[parsed.domain]:
+                self._db[parsed.domain][parsed.subdomain] = []
+            if parsed.path not in self._db[parsed.domain][parsed.subdomain]:
+                self._db[parsed.domain][parsed.subdomain].append(parsed.path)
+                LOG.debug("added: %s", parsed)
+                return parsed
+        return None
+
+    @property
+    def domains(self) -> Generator[str, None, None]:
+        """All known domains.
+
+        Args:
+            None
+
+        Yields:
+            Domains in URL collection.
+        """
+        yield from self._db
+
+    def remove_url(self, url: str) -> bool:
+        """Remove a URL from the list of known URLs.
+
+        Args:
+            url: URL to remove.
+
+        Returns:
+            True if the URL was removed otherwise False.
+        """
+        parsed = self._parse_url(url)
+        if parsed is None:
+            LOG.debug("failed to parse and remove: %r", url)
+            return False
+
+        try:
+            self._db[parsed.domain][parsed.subdomain].remove(parsed.path)
+        except (KeyError, ValueError):
+            LOG.debug("failed to remove unknown url: %r", url)
+            return False
+
+        # remove empty domain/subdomain entries
+        if not self._db[parsed.domain][parsed.subdomain]:
+            del self._db[parsed.domain][parsed.subdomain]
+        if not self._db[parsed.domain]:
+            del self._db[parsed.domain]
+
+        return True
+
+    @classmethod
+    def load_yml(cls, src: Path) -> Optional["UrlCollection"]:
+        """Load UrlCollection from a YML file.
+
+        Args:
+            src: File to load.
+
+        Returns:
+            UrlCollection.
+        """
+        data = next(load_input([src], allow_empty=True), None)
+        if data is not None:
+            return cls(data)
+        return None
+
+    def save_yml(self, dst: Path) -> None:
+        """Save UrlCollection to a YML file.
+
+        Args:
+            dst: File to save to.
+
+        Returns:
+            None
+        """
+        with dst.open("w") as out_fp:
+            dump(self._db, out_fp, Dumper=SafeDumper, default_style="'")
+
+
+def parse_args(argv: Optional[List[str]] = None) -> Namespace:
+    """Argument parsing"""
+    parser = ArgumentParser(description="Manage URL collections used by Site-scout.")
+    parser.add_argument("url_db", type=Path, help="YML file containing data.")
+    parser.add_argument("-d", "--display-list", action="store_true")
+    level_map = {"INFO": INFO, "DEBUG": DEBUG}
+    parser.add_argument(
+        "--log-level",
+        choices=sorted(level_map),
+        default="INFO",
+        help="Configure console logging (default: %(default)s).",
+    )
+    group = parser.add_mutually_exclusive_group(required=False)
+    group.add_argument(
+        "-l",
+        "--url-list",
+        type=Path,
+        help="File containing line separated list of URLs.",
+    )
+    group.add_argument("-r", "--remove", help="Remove single URL.")
+    group.add_argument("-u", "--url", help="Add single URL.")
+    args = parser.parse_args(argv)
+
+    args.log_level = level_map[args.log_level]
+
+    if args.url_list and not args.url_list.is_file():
+        parser.error(f"{args.url_list} is not a file.")
+
+    return args
+
+
+# pylint: disable=too-many-branches
+def main(argv: Optional[List[str]] = None) -> int:
+    """Main function"""
+    args = parse_args(argv)
+    init_logging(args.log_level)
+
+    try:
+        # load data
+        if args.url_db and args.url_db.is_file():
+            LOG.info("Loading '%s'...", args.url_db)
+            urls = UrlCollection.load_yml(args.url_db)
+            if urls is None:
+                return 1
+        else:
+            urls = UrlCollection()
+
+        # update data
+        changed = False
+        if args.url:
+            added = urls.add_url(args.url)
+            if added is not None:
+                LOG.info("Added '%s'", added)
+                changed = True
+        elif args.url_list:
+            LOG.info("Scanning URL(s) in '%s'...", args.url_list)
+            added_count = urls.add_list(args.url_list)
+            LOG.info("Added %s URL(s).", f"{added_count:,d}")
+            changed = added_count > 0
+        elif args.remove:
+            changed = urls.remove_url(args.remove)
+            if changed:
+                LOG.info("Removed 1 URL.")
+
+        # save data
+        if changed:
+            LOG.info("Saving '%s'...", args.url_db)
+            urls.save_yml(args.url_db)
+        else:
+            LOG.info("No change made.")
+
+        # show current list of URLs
+        if args.display_list:
+            LOG.info("Known URLs: %s", f"{len(urls):,d}")
+            for count, entry in enumerate(urls, start=1):
+                LOG.info("%04d: %s", count, entry)
+
+        # show unparsable input
+        if urls.unparsable:
+            LOG.warning("Unparsable entries: %s", f"{len(urls.unparsable):,d}")
+            for unparsable in urls.unparsable:
+                LOG.info("- '%s'", unparsable)
+
+        # show summary
+        try:
+            file_size = args.url_db.stat().st_size
+        except FileNotFoundError:
+            file_size = 0
+        LOG.info(
+            "%s URLs (%s domains) in db (%s bytes).",
+            f"{len(urls):,d}",
+            f"{sum(1 for _ in urls.domains):,d}",
+            f"{file_size:,d}",
+        )
+
+    except KeyboardInterrupt:  # pragma: no cover
+        LOG.warning("Aborting...")
+
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
