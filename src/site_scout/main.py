86855c3795176e678d863ffe8b7a501b9b33efe7
Tyson Smith <tyson.w.smith@gmail.com>
2024-09-04 09:44:45 -0700

    [ci] Update linters

--- src/site_scout/main.py
+++ src/site_scout/main.py
@@ -1,6 +1,8 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
+from __future__ import annotations
+
 from logging import DEBUG, basicConfig, getLogger
 from pathlib import Path
 from tempfile import NamedTemporaryFile
