47785a8079d1fd1359215490069b92cfb2e00f86
Tyson Smith <tyson.w.smith@gmail.com>
2025-04-14 13:17:55 -0700

    refactor: simplify explorer
    - make ExplorerStatus immutable dataclass
    - remove helper functions and access status directly
    - remove PENDING state
    - replace lock with queue

--- src/site_scout/explorer.py
+++ src/site_scout/explorer.py
@@ -3,9 +3,11 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 from __future__ import annotations
 
-from enum import Enum, auto, unique
+from dataclasses import dataclass
+from enum import Enum, auto
 from logging import ERROR, WARNING, getLogger
-from threading import Event, Lock, Thread
+from queue import Queue
+from threading import Event, Thread
 from time import perf_counter
 from typing import TYPE_CHECKING
 
@@ -22,11 +24,9 @@ LOAD_WAIT = 10
 LOG = getLogger(__name__)
 
 
-@unique
 class State(Enum):
     """State of Explorer"""
 
-    PENDING = auto()
     INITIALIZING = auto()
     LOADING = auto()
     NOT_FOUND = auto()
@@ -47,32 +47,30 @@ PAGE_LOAD_FAILURES = frozenset(
 )
 
 
+@dataclass(eq=False, frozen=True)
 class ExplorerStatus:
     """Used to collect status data from PageExplorer."""
 
-    __slots__ = ("explore_duration", "load_duration", "lock", "state", "url_loaded")
-
-    def __init__(self) -> None:
-        self.explore_duration: float | None = None
-        self.load_duration: float | None = None
-        self.lock = Lock()
-        self.state: State = State.PENDING
-        self.url_loaded: str | None = None
+    explore_duration: float | None
+    load_duration: float | None
+    state: State
+    url_loaded: str | None
 
 
 class Explorer:
     """PageExplorer wrapper to enable use via SiteScout."""
 
-    __slots__ = ("_can_skip", "_status", "_thread")
+    __slots__ = ("_can_skip", "_queue", "_status", "_thread")
 
     def __init__(self, binary: Path, port: int, url: str) -> None:
         # init is used to wait for PageExplorer to connect
         init = Event()
         self._can_skip = Event()
-        self._status = ExplorerStatus()
+        self._queue: Queue[ExplorerStatus] = Queue(maxsize=1)
+        self._status: ExplorerStatus | None = None
         self._thread = Thread(
             target=self._run,
-            args=(binary, port, url, self._status, self._can_skip, init),
+            args=(binary, port, url, self._queue, self._can_skip, init),
         )
         self._thread.start()
         # wait for PageExplorer to connect before continuing
@@ -86,7 +84,7 @@ class Explorer:
         self.close()
 
     def close(self) -> None:
-        """Unblock and join PageExplorer thread.
+        """Unblock and join PageExplorer thread and set status.
 
         Args:
             None
@@ -94,22 +92,13 @@ class Explorer:
         Returns:
             None
         """
-        # disable delay to unblock shutdown
-        self._can_skip.set()
-        # this should NEVER timeout, if it does there is a bug
-        self._thread.join(timeout=60)
-
-    def explore_duration(self) -> float | None:
-        """Amount of time in seconds spent exploring content.
-
-        Args:
-            None
-
-        Returns:
-            Time in seconds if available otherwise None.
-        """
-        with self._status.lock:
-            return self._status.explore_duration
+        # check if close() was already called
+        if self._status is None:
+            # disable delay to unblock shutdown
+            self._can_skip.set()
+            # this should NEVER timeout, if it does there is a bug
+            self._status = self._queue.get(timeout=60)
+            self._thread.join()
 
     def is_running(self) -> bool:
         """Check if PageExplorer thread is running.
@@ -122,49 +111,26 @@ class Explorer:
         """
         return self._thread.is_alive()
 
-    def load_duration(self) -> float | None:
-        """Amount of time in seconds spent loading content.
-
-        Args:
-            None
-
-        Returns:
-            Time in seconds if available otherwise None.
-        """
-        with self._status.lock:
-            return self._status.load_duration
-
-    def state(self) -> State:
-        """Current state of the explorer.
-
-        Args:
-            None
-
-        Returns:
-            Current state.
-        """
-        with self._status.lock:
-            return self._status.state
-
     @property
-    def url_loaded(self) -> str | None:
-        """Gets the URL of the page initially loaded.
+    def status(self) -> ExplorerStatus:
+        """Final status of Explorer execution. Explorer.close() must be called first.
 
         Args:
             None
 
         Returns:
-            The URL if it is available otherwise None.
+            Status of Explorer.
         """
-        with self._status.lock:
-            return self._status.url_loaded
+        if self._status is None:
+            raise RuntimeError("Explorer.close() not called")
+        return self._status
 
     @staticmethod
     def _run(
         binary: Path,
         port: int,
         url: str,
-        status: ExplorerStatus,
+        queue: Queue[ExplorerStatus],
         can_skip: Event,
         init: Event,
     ) -> None:
@@ -174,74 +140,67 @@ class Explorer:
             binary: Binary launched.
             port: Browser driver listening port.
             url: Url to visit.
-            status: Status tracker.
+            queue:
             can_skip: Used to delay execution.
             init: Used to delay initialization of Explorer until PageExplorer is ready.
 
         Returns:
             None.
         """
+
+        def _wait(seconds: float) -> None:  # pragma: no cover
+            assert seconds >= 0
+            can_skip.wait(seconds)
+
+        explore_duration = None
+        load_duration = None
+        url_loaded = None
+        state = State.INITIALIZING
         try:
-
-            def custom_wait(seconds: float) -> None:  # pragma: no cover
-                assert seconds >= 0
-                can_skip.wait(seconds)
-
-            with status.lock:
-                status.state = State.INITIALIZING
             with PageExplorer(binary=binary, port=port) as explorer:
                 # indicate PageExplorer has been initialized and connected to browser
                 init.set()
                 # attempt to navigate and load page
-                with status.lock:
-                    status.state = State.LOADING
+                state = State.LOADING
                 start_time = perf_counter()
                 get_result = explorer.get(url)
                 title = explorer.title
                 # verify page load
                 if not get_result:
                     if title == "Server Not Found":
-                        with status.lock:
-                            status.state = State.NOT_FOUND
+                        state = State.NOT_FOUND
                     elif title == "Problem loading page":
-                        with status.lock:
-                            status.state = State.LOAD_FAILURE
+                        state = State.LOAD_FAILURE
                     else:
-                        with status.lock:
-                            status.state = State.UNHANDLED_ERROR
+                        state = State.UNHANDLED_ERROR
                         LOG.warning("Failed to get: %s (%r)", url, title)
                     # the browser is running but page cannot be loaded
                     return
-                duration = perf_counter() - start_time
-                with status.lock:
-                    status.load_duration = duration
-                    status.url_loaded = explorer.current_url
-                    LOG.debug("loaded: %r (%r)", title, status.url_loaded)
+                load_duration = perf_counter() - start_time
+                url_loaded = explorer.current_url
+                LOG.debug("loaded: %r (%r)", title, url_loaded)
                 # wait for more content to load/render
                 can_skip.wait(timeout=LOAD_WAIT)
-                with status.lock:
-                    status.state = State.SKIP_CONTENT
                 # attempt to find and activate "skip to content" link
+                state = State.SKIP_CONTENT
                 explorer.skip_to_content()
-                with status.lock:
-                    status.state = State.EXPLORING
                 # interact with content
+                state = State.EXPLORING
                 start_time = perf_counter()
-                if not explorer.explore(wait_cb=custom_wait):
+                if not explorer.explore(wait_cb=_wait):
                     # failed to execute all explore instructions
                     return
-                duration = perf_counter() - start_time
-                with status.lock:
-                    status.explore_duration = duration
-                    status.state = State.CLOSING
+                explore_duration = perf_counter() - start_time
                 # attempt to close the browser
+                state = State.CLOSING
                 explorer.close_browser(wait=10)
                 if not explorer.is_connected():
-                    with status.lock:
-                        status.state = State.CLOSED
+                    state = State.CLOSED
         except ExplorerError:
             LOG.debug("ExplorerError detected, aborting...")
         finally:
             init.set()
-            with status.lock:
-                LOG.debug("final state: %s", status.state.name)
+            LOG.debug("final state: %s", state.name)
+            queue.put(
+                ExplorerStatus(explore_duration, load_duration, state, url_loaded)
+            )
